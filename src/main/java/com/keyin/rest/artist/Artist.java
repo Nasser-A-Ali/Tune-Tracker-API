3d950aba5db1c17715b2806c3e96cf3a42e8f581
SearchingSteve <155838264+SearchingSteve@users.noreply.github.com>
2025-02-15 12:04:27 -0330

    feat(models): add album, artist, & song JPA models
    Basic JPA models added for Music App API, following backend naming.
    
    - Album.java to store albums
    - Artist.java to store Artists
    - Song.java to store songs

--- src/main/java/com/keyin/rest/artist/Artist.java
+++ src/main/java/com/keyin/rest/artist/Artist.java
@@ -1,4 +1,24 @@
 package com.keyin.rest.artist;
 
+
+import com.keyin.rest.album.Album;
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
 public class Artist {
+    @Id
+    @SequenceGenerator(name = "artist_sequence", sequenceName = "artist_sequence", allocationSize = 1, initialValue = 1)
+    @GeneratedValue(generator = "artist_sequence")
+    private long id;
+
+    private String name;
+    private String genre;
+
+    @OneToMany // An artist can have many albums
+    private List<Album> albums;
+
+
+
+
 }
--- src/main/java/com/keyin/rest/song/Song.java
+++ src/main/java/com/keyin/rest/song/Song.java
@@ -1,4 +1,27 @@
 package com.keyin.rest.song;
 
+import com.keyin.rest.album.Album;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
 public class Song {
+    @Id
+    @SequenceGenerator(name = "song_sequence", sequenceName = "song_sequence", allocationSize = 1, initialValue = 1)
+    @GeneratedValue(generator = "song_sequence")
+    private long id;
+
+    private String title;
+    private String genre;
+    private int duration;
+    private LocalDate releaseDate;
+
+    @ManyToMany // A song can exist in many albums (ex. singles & full albums)
+            //   & an album can have many sounds
+    private List<Album> albums;
+
+
+
 }
