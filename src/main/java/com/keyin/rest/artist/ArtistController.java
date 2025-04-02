6a4e2002db0e97e86628d8a5c73e5a6ac60fae09
SearchingSteve <155838264+SearchingSteve@users.noreply.github.com>
2025-04-02 14:31:19 -0230

    fix: Update artist enpoints
    - Update artist_search by adding findByDebutYear search
    - Remove song_title search from artist search
    - Change find findByDebutYear param from int to Integer to handle null exceptions
    - Change application properties mysql db name

--- src/main/java/com/keyin/rest/artist/ArtistController.java
+++ src/main/java/com/keyin/rest/artist/ArtistController.java
@@ -27,12 +27,12 @@ public class ArtistController {
 
     @GetMapping("/artist_search")
     public List<Artist> searchArtists(
-            @RequestParam(value = "song_title", required = false) String title,
-            @RequestParam(value = "artist_id", required = false) Long artistId,
+            @RequestParam(value = "artist_id", required = false) Long artistId, // unncesesary (/artist/{id}) does same thing
+            @RequestParam(value = "debut_year", required = false) Integer debutYear,
             @RequestParam(value = "genre", required = false) String genre) {
 
-        if (title != null) {
-            return List.of(artistService.findByName(title));
+        if (debutYear != null) {
+            return artistService.findByDebutYear(debutYear);
         } else if (artistId != null) {
             return List.of(artistService.findById(artistId));
         } else if (genre != null) {
@@ -44,18 +44,18 @@ public class ArtistController {
 
 
     @PostMapping("/artist")
-        public Artist createArtist(@RequestBody Artist artist){
-            return artistService.createArtist(artist);
-        }
+    public Artist createArtist(@RequestBody Artist artist) {
+        return artistService.createArtist(artist);
+    }
 
     @PutMapping("/artist/{id}")
     public Artist updateArtist(@PathVariable long id,
-                               @RequestBody Artist artist){
+                               @RequestBody Artist artist) {
         return artistService.updateArtist(id, artist);
     }
 
     @DeleteMapping("/artist/{id}")
-    public void deleteArtistById(@PathVariable long id){
+    public void deleteArtistById(@PathVariable long id) {
         artistService.deleteArtistById(id);
     }
 
--- src/main/java/com/keyin/rest/artist/ArtistRepository.java
+++ src/main/java/com/keyin/rest/artist/ArtistRepository.java
@@ -1,14 +1,17 @@
 package com.keyin.rest.artist;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ArtistRepository extends CrudRepository<Artist, Long> {
     Artist findByName(String name);
-    List<Artist> findByDebutYear(int year);
+
+    List<Artist> findByDebutYear(Integer year);
+
     List<Artist> findByGenre(String genre);
+
     List<Artist> findByCountry(String country);
 }
