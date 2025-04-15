e539d7666d29407b5e88e10b9b1f6dbbd706d886
SearchingSteve <155838264+SearchingSteve@users.noreply.github.com>
2025-04-15 13:35:51 -0230

    Album package comments
    JavaDocs style comments for album package files:
    - Artist
    - ArtistController
    - ArtistRepository
    - ArtistService

--- src/main/java/com/keyin/rest/artist/Artist.java
+++ src/main/java/com/keyin/rest/artist/Artist.java
@@ -1,6 +1,5 @@
 package com.keyin.rest.artist;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.keyin.rest.album.Album;
 import com.keyin.rest.song.Song;
@@ -9,79 +8,163 @@ import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Entity class representing an Artist in the music database.
+ * An artist can have many albums and songs.
+ */
 @Entity
 public class Artist {
+    /** Unique identifier for the artist */
     @Id
     @SequenceGenerator(name = "artist_sequence", sequenceName = "artist_sequence", allocationSize = 1, initialValue = 1)
     @GeneratedValue(generator = "artist_sequence")
     private long id;
 
+    /** The name of the artist */
     private String name;
+
+    /** The year the artist debuted */
     private int debutYear;
+
+    /** The primary genre of the artist */
     private String genre;
+
+    /** The country of origin for the artist */
     private String country;
 
-    @OneToMany(mappedBy = "artist", cascade = {CascadeType.PERSIST, CascadeType.MERGE}) // An artist can have many albums
+    /** The list of albums created by the artist */
+    @OneToMany(mappedBy = "artist", cascade = { CascadeType.PERSIST, CascadeType.MERGE }) // An artist can have many
+                                                                                          // albums
     @JsonIgnore
     private List<Album> albums = new ArrayList<>();
 
-    @OneToMany(mappedBy = "artist", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    /** The list of songs created by the artist */
+    @OneToMany(mappedBy = "artist", cascade = { CascadeType.PERSIST, CascadeType.MERGE })
     @JsonIgnore
     private List<Song> songs = new ArrayList<>();
 
-
+    /**
+     * Gets the unique identifier of the artist.
+     *
+     * @return the artist's ID
+     */
     public long getId() {
         return id;
     }
 
+    /**
+     * Sets the unique identifier of the artist.
+     *
+     * @param id the artist's ID
+     */
     public void setId(long id) {
         this.id = id;
     }
 
+    /**
+     * Gets the name of the artist.
+     *
+     * @return the artist's name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the artist.
+     *
+     * @param name the artist's name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the debut year of the artist.
+     *
+     * @return the artist's debut year
+     */
     public int getDebutYear() {
         return debutYear;
     }
 
+    /**
+     * Sets the debut year of the artist.
+     *
+     * @param debutYear the artist's debut year
+     */
     public void setDebutYear(int debutYear) {
         this.debutYear = debutYear;
     }
 
+    /**
+     * Gets the genre of the artist.
+     *
+     * @return the artist's genre
+     */
     public String getGenre() {
         return genre;
     }
 
+    /**
+     * Sets the genre of the artist.
+     *
+     * @param genre the artist's genre
+     */
     public void setGenre(String genre) {
         this.genre = genre;
     }
 
+    /**
+     * Gets the country of origin for the artist.
+     *
+     * @return the artist's country
+     */
     public String getCountry() {
         return country;
     }
 
+    /**
+     * Sets the country of origin for the artist.
+     *
+     * @param country the artist's country
+     */
     public void setCountry(String country) {
         this.country = country;
     }
 
+    /**
+     * Gets the list of albums created by the artist.
+     *
+     * @return the list of albums
+     */
     public List<Album> getAlbums() {
         return albums;
     }
 
+    /**
+     * Sets the list of albums created by the artist.
+     *
+     * @param albums the list of albums
+     */
     public void setAlbums(List<Album> albums) {
         this.albums = albums;
     }
 
+    /**
+     * Gets the list of songs created by the artist.
+     *
+     * @return the list of songs
+     */
     public List<Song> getSongs() {
         return songs;
     }
 
+    /**
+     * Sets the list of songs created by the artist.
+     *
+     * @param songs the list of songs
+     */
     public void setSongs(List<Song> songs) {
         this.songs = songs;
     }
--- src/main/java/com/keyin/rest/artist/ArtistService.java
+++ src/main/java/com/keyin/rest/artist/ArtistService.java
@@ -1,52 +1,100 @@
 package com.keyin.rest.artist;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Service class for managing Artist entities.
+ * Handles business logic and data persistence operations for artists.
+ */
 @Service
 public class ArtistService {
+    /** Repository for artist data persistence */
     @Autowired
     private ArtistRepository artistRepository;
 
-
-    // Create
+    /**
+     * Creates a new artist.
+     *
+     * @param newArtist The artist to create
+     * @return The created artist
+     */
     public Artist createArtist(Artist newArtist) {
         return artistRepository.save(newArtist);
     }
 
-
-    // Find/Read
+    /**
+     * Retrieves an artist by their ID.
+     *
+     * @param id The ID of the artist to retrieve
+     * @return The artist with the specified ID, or null if not found
+     */
     public Artist findById(long id) {
         Optional<Artist> divisionOptional = artistRepository.findById(id);
         return divisionOptional.orElse(null);
     }
 
+    /**
+     * Retrieves an artist by their name.
+     *
+     * @param name The name of the artist to retrieve
+     * @return The artist with the specified name, or null if not found
+     */
     public Artist findByName(String name) {
         return artistRepository.findByName(name);
     }
 
-    public List<Artist> findAll(){
+    /**
+     * Retrieves all artists from the database.
+     *
+     * @return List of all artists
+     */
+    public List<Artist> findAll() {
         return (List<Artist>) artistRepository.findAll();
     }
 
+    /**
+     * Searches for artists by debut year.
+     *
+     * @param year The debut year to search for
+     * @return List of artists who debuted in the specified year
+     */
     public List<Artist> findByDebutYear(int year) {
         return artistRepository.findByDebutYear(year);
     }
 
+    /**
+     * Searches for artists by genre.
+     *
+     * @param genre The genre to search for
+     * @return List of artists in the specified genre
+     */
     public List<Artist> findByGenre(String genre) {
         return artistRepository.findByGenre(genre);
     }
 
+    /**
+     * Searches for artists by country.
+     *
+     * @param country The country to search for
+     * @return List of artists from the specified country
+     */
     public List<Artist> findByCountry(String country) {
         return artistRepository.findByCountry(country);
     }
 
-
-    // Update
+    /**
+     * Updates an existing artist.
+     * Only updates fields that are provided in the updatedArtist parameter.
+     * Maintains existing values for fields that are not provided.
+     *
+     * @param id The ID of the artist to update
+     * @param updatedArtist The updated artist data
+     * @return The updated artist, or null if not found
+     */
     public Artist updateArtist(long id, Artist updatedArtist) {
         Optional<Artist> artistToUpdateOptional = artistRepository.findById(id);
     
@@ -79,14 +127,21 @@ public class ArtistService {
         return null;
     }
     
-    // Delete
+    /**
+     * Deletes an artist by their ID.
+     *
+     * @param id The ID of the artist to delete
+     */
     public void deleteArtistById(long id) {
         artistRepository.deleteById(id);
     }
 
+    /**
+     * Deletes an artist by their name.
+     *
+     * @param name The name of the artist to delete
+     */
     public void deleteArtistByName(String name) {
         artistRepository.delete(findByName(name));
     }
-
-
 }
