31c7bd0202e411dd8441fee71d8840a89d587d30
SearchingSteve <155838264+SearchingSteve@users.noreply.github.com>
2025-04-15 13:31:00 -0230

    Album package comments
    JavaDocs style comments for album package files:
    - Album
    - AlbumController
    - AlbumRepository
    - AlbumServicee

--- src/main/java/com/keyin/rest/album/Album.java
+++ src/main/java/com/keyin/rest/album/Album.java
@@ -3,76 +3,146 @@ package com.keyin.rest.album;
 import com.keyin.rest.artist.Artist;
 import com.keyin.rest.song.Song;
 import jakarta.persistence.*;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.util.List;
 
+/**
+ * Entity class representing an Album in the music database.
+ * An album belongs to one artist and can contain multiple songs.
+ */
 @Entity
 public class Album {
+    /** Unique identifier for the album */
     @Id
     @SequenceGenerator(name = "album_sequence", sequenceName = "album_sequence", allocationSize = 1, initialValue = 1)
     @GeneratedValue(generator = "album_sequence")
     private long id;
 
+    /** The title of the album */
     private String title;
+
+    /** The year the album was released */
     private int releaseYear;
+
+    /** The musical genre of the album */
     private String genre;
 
+    /** The artist who created the album */
     @ManyToOne
     @JoinColumn(name = "artist_id", nullable = false)
     @JsonIgnoreProperties({ "albums", "songs" })
     private Artist artist;
 
+    /** The list of songs included in the album */
     @ManyToMany
     @JoinTable(name = "album_song", joinColumns = @JoinColumn(name = "album_id"), inverseJoinColumns = @JoinColumn(name = "song_id"))
     private List<Song> listOfSongs;
 
-    // Getters and Setters
+    /**
+     * Gets the unique identifier of the album.
+     *
+     * @return the album's ID
+     */
     public long getId() {
         return id;
     }
 
+    /**
+     * Sets the unique identifier of the album.
+     *
+     * @param id the album's ID
+     */
     public void setId(long id) {
         this.id = id;
     }
 
+    /**
+     * Gets the title of the album.
+     *
+     * @return the album's title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets the title of the album.
+     *
+     * @param title the album's title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Gets the artist who created the album.
+     *
+     * @return the album's artist
+     */
     public Artist getArtist() {
         return artist;
     }
 
+    /**
+     * Sets the artist who created the album.
+     *
+     * @param artist the album's artist
+     */
     public void setArtist(Artist artist) {
         this.artist = artist;
     }
 
+    /**
+     * Gets the release year of the album.
+     *
+     * @return the album's release year
+     */
     public int getReleaseYear() {
         return releaseYear;
     }
 
+    /**
+     * Sets the release year of the album.
+     *
+     * @param releaseYear the album's release year
+     */
     public void setReleaseYear(int releaseYear) {
         this.releaseYear = releaseYear;
     }
 
+    /**
+     * Gets the genre of the album.
+     *
+     * @return the album's genre
+     */
     public String getGenre() {
         return genre;
     }
 
+    /**
+     * Sets the genre of the album.
+     *
+     * @param genre the album's genre
+     */
     public void setGenre(String genre) {
         this.genre = genre;
     }
 
+    /**
+     * Gets the list of songs in the album.
+     *
+     * @return the list of songs
+     */
     public List<Song> getListOfSongs() {
         return listOfSongs;
     }
 
+    /**
+     * Sets the list of songs in the album.
+     *
+     * @param listOfSongs the list of songs
+     */
     public void setListOfSongs(List<Song> listOfSongs) {
         this.listOfSongs = listOfSongs;
     }
--- src/main/java/com/keyin/rest/album/AlbumService.java
+++ src/main/java/com/keyin/rest/album/AlbumService.java
@@ -6,45 +6,105 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Service class for managing Album entities.
+ * Handles business logic and data persistence operations for albums.
+ */
 @Service
 public class AlbumService {
 
+    /** Repository for album data persistence */
     @Autowired
     private AlbumRepository albumRepository;
 
+    /**
+     * Retrieves all albums from the database.
+     *
+     * @return List of all albums
+     */
     public List<Album> getAllAlbums() {
         return (List<Album>) albumRepository.findAll();
     }
 
+    /**
+     * Retrieves an album by its ID.
+     *
+     * @param id The ID of the album to retrieve
+     * @return The album with the specified ID, or null if not found
+     */
     public Album getAlbumById(long id) {
         Optional<Album> albumOptional = albumRepository.findById(id);
         return albumOptional.orElse(null);
     }
 
+    /**
+     * Searches for albums by title.
+     *
+     * @param title The title to search for
+     * @return List of albums with the specified title
+     */
     public List<Album> getAlbumByTitle(String title) {
         return albumRepository.findByTitle(title);
     }
 
+    /**
+     * Searches for albums by artist ID.
+     *
+     * @param artistId The artist ID to search for
+     * @return List of albums by the specified artist
+     */
     public List<Album> getAlbumByArtistId(long artistId) {
         return albumRepository.findByArtistId(artistId);
     }
 
+    /**
+     * Searches for albums by release year.
+     *
+     * @param releaseYear The release year to search for
+     * @return List of albums released in the specified year
+     */
     public List<Album> getAlbumByReleaseYear(int releaseYear) {
         return albumRepository.findByReleaseYear(releaseYear);
     }
 
+    /**
+     * Searches for albums by genre.
+     *
+     * @param genre The genre to search for
+     * @return List of albums in the specified genre
+     */
     public List<Album> getAlbumByGenre(String genre) {
         return albumRepository.findByGenre(genre);
     }
 
+    /**
+     * Deletes an album by its ID.
+     *
+     * @param id The ID of the album to delete
+     */
     public void deleteAlbumById(long id) {
         albumRepository.deleteById(id);
     }
 
+    /**
+     * Creates a new album.
+     *
+     * @param newAlbum The album to create
+     * @return The created album
+     */
     public Album createAlbum(Album newAlbum) {
         return albumRepository.save(newAlbum);
     }
 
+    /**
+     * Updates an existing album.
+     * Only updates fields that are provided in the updatedAlbum parameter.
+     * Maintains existing values for fields that are not provided.
+     *
+     * @param id           The ID of the album to update
+     * @param updatedAlbum The updated album data
+     * @return The updated album, or null if not found
+     */
     public Album updateAlbum(long id, Album updatedAlbum) {
         Optional<Album> albumToUpdateOptional = albumRepository.findById(id);
 
@@ -56,7 +116,7 @@ public class AlbumService {
                     .setTitle(updatedAlbum.getTitle() != null ? updatedAlbum.getTitle() : albumToUpdate.getTitle());
             albumToUpdate
                     .setArtist(updatedAlbum.getArtist() != null ? updatedAlbum.getArtist() : albumToUpdate.getArtist());
-                    
+
             albumToUpdate.setReleaseYear(updatedAlbum.getReleaseYear() >= 0 ? updatedAlbum.getReleaseYear()
                     : albumToUpdate.getReleaseYear());
 
