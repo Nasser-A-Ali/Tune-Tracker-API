5b030c8d8434f01a5ce021924c8f2cc25033c2e2
SearchingSteve <155838264+SearchingSteve@users.noreply.github.com>
2025-04-15 13:41:47 -0230

    Song package comments
    JavaDocs style comments for song package files:
    - Song
    - SongController
    - SongRepository
    - SongService

--- src/main/java/com/keyin/rest/song/Song.java
+++ src/main/java/com/keyin/rest/song/Song.java
@@ -7,81 +7,162 @@ import jakarta.persistence.*;
 
 import java.util.List;
 
+/**
+ * Entity class representing a Song in the music database.
+ * A song belongs to one artist and can be included in multiple albums.
+ */
 @Entity
 public class Song {
+    /** Unique identifier for the song */
     @Id
     @SequenceGenerator(name = "song_sequence", sequenceName = "song_sequence", allocationSize = 1, initialValue = 1)
     @GeneratedValue(generator = "song_sequence")
     private long id;
 
+    /** The title of the song */
     private String title;
+
+    /** The genre of the song */
     private String genre;
+
+    /** The duration of the song in seconds */
     private int duration;
+
+    /** The year the song was released */
     private int releaseYear;
 
+    /** The artist who created the song */
     @ManyToOne
     @JoinColumn(name = "artist_id", nullable = false)
     private Artist artist;
 
-
-    // A song can exist in many albums (ex. singles & full albums)
-    // & an album can have many sounds
+    /** The list of albums that include this song */
     @ManyToMany(mappedBy = "listOfSongs")
     @JsonIgnore
     private List<Album> albums;
 
+    /**
+     * Gets the unique identifier of the song.
+     *
+     * @return the song's ID
+     */
     public long getId() {
         return id;
     }
 
+    /**
+     * Sets the unique identifier of the song.
+     *
+     * @param id the song's ID
+     */
     public void setId(long id) {
         this.id = id;
     }
 
+    /**
+     * Gets the title of the song.
+     *
+     * @return the song's title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets the title of the song.
+     *
+     * @param title the song's title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Gets the artist who created the song.
+     *
+     * @return the song's artist
+     */
     public Artist getArtist() {
         return artist;
     }
 
+    /**
+     * Sets the artist who created the song.
+     *
+     * @param artist the song's artist
+     */
     public void setArtist(Artist artist) {
         this.artist = artist;
     }
 
+    /**
+     * Gets the genre of the song.
+     *
+     * @return the song's genre
+     */
     public String getGenre() {
         return genre;
     }
 
+    /**
+     * Sets the genre of the song.
+     *
+     * @param genre the song's genre
+     */
     public void setGenre(String genre) {
         this.genre = genre;
     }
 
+    /**
+     * Gets the duration of the song in seconds.
+     *
+     * @return the song's duration
+     */
     public int getDuration() {
         return duration;
     }
 
+    /**
+     * Sets the duration of the song in seconds.
+     *
+     * @param duration the song's duration
+     */
     public void setDuration(int duration) {
         this.duration = duration;
     }
 
+    /**
+     * Gets the release year of the song.
+     *
+     * @return the song's release year
+     */
     public int getReleaseYear() {
         return releaseYear;
     }
 
+    /**
+     * Sets the release year of the song.
+     *
+     * @param releaseYear the song's release year
+     */
     public void setReleaseYear(int releaseYear) {
         this.releaseYear = releaseYear;
     }
 
+    /**
+     * Gets the list of albums that include this song.
+     *
+     * @return the list of albums
+     */
     public List<Album> getAlbums() {
         return albums;
     }
 
+    /**
+     * Sets the list of albums that include this song.
+     *
+     * @param albums the list of albums
+     */
     public void setAlbums(List<Album> albums) {
         this.albums = albums;
     }
--- src/main/java/com/keyin/rest/song/SongService.java
+++ src/main/java/com/keyin/rest/song/SongService.java
@@ -6,45 +6,105 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Service class for managing Song entities.
+ * Handles business logic and data persistence operations for songs.
+ */
 @Service
 public class SongService {
+    /** Repository for song data persistence */
     @Autowired
     private SongRepository songRepository;
 
+    /**
+     * Retrieves all songs from the database.
+     *
+     * @return List of all songs
+     */
     public List<Song> getAllSongs() {
         return (List<Song>) songRepository.findAll();
     }
 
+    /**
+     * Retrieves a song by its ID.
+     *
+     * @param id The ID of the song to retrieve
+     * @return The song with the specified ID, or null if not found
+     */
     public Song getSongById(long id) {
         Optional<Song> songOptional = songRepository.findById(id);
-
         return songOptional.orElse(null);
     }
 
+    /**
+     * Searches for songs by title.
+     *
+     * @param title The title to search for
+     * @return List of songs matching the title
+     */
     public List<Song> getSongByTitle(String title) {
         return songRepository.findByTitle(title);
     }
 
+    /**
+     * Searches for songs by genre.
+     *
+     * @param genre The genre to search for
+     * @return List of songs matching the genre
+     */
     public List<Song> getSongByGenre(String genre) {
         return songRepository.findByGenre(genre);
     }
 
+    /**
+     * Searches for songs by release year.
+     *
+     * @param releaseYear The release year to search for
+     * @return List of songs matching the release year
+     */
     public List<Song> getSongByReleaseYear(int releaseYear) {
         return songRepository.findByReleaseYear(releaseYear);
     }
 
+    /**
+     * Searches for songs by artist ID.
+     *
+     * @param artistId The artist ID to search for
+     * @return List of songs by the specified artist
+     */
     public List<Song> getSongByArtistId(long artistId) {
         return songRepository.findByArtistId(artistId);
     }
 
+    /**
+     * Deletes a song by its ID.
+     *
+     * @param id The ID of the song to delete
+     */
     public void deleteSongById(long id) {
         songRepository.deleteById(id);
     }
 
+    /**
+     * Creates a new song.
+     *
+     * @param newSong The song to create
+     * @return The created song
+     */
     public Song createSong(Song newSong) {
         return songRepository.save(newSong);
     }
 
+    /**
+     * Updates an existing song.
+     * Only non-null and non-empty fields from the updated song will be applied.
+     * For numeric fields (duration, release year), only positive values will be
+     * applied.
+     *
+     * @param id          The ID of the song to update
+     * @param updatedSong The updated song data
+     * @return The updated song, or null if the song doesn't exist
+     */
     public Song updateSong(long id, Song updatedSong) {
         Optional<Song> songToUpdateOptional = songRepository.findById(id);
 
@@ -85,5 +145,4 @@ public class SongService {
 
         return null;
     }
-
 }
